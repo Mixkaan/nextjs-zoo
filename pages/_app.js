049ef9162d313f70ea586d74ed0b87197cddf8a4
victor manuel carrillo rojas <mixkaanexotics@gmail.com>
2021-05-20 16:18:06 -0500

    primer commit & deploy de la plicacion ZOOLOGISTICA hecha con next js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
-import '../styles/globals.css'
+//import 'tailwindcss/tailwind.css'
+import '../sass/main.sass'
 
-function MyApp({ Component, pageProps }) {
+
+function MyApp( { Component, pageProps }) {
+
   return <Component {...pageProps} />
 }
 
